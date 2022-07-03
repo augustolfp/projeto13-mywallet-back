d121a1f6ef95cceda0650f2ab92331529a540cd1
Augusto Lopes <augustolfp@gmail.com>
2022-07-02 23:18:47 -0300

    Configures sign-up controller

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,26 +1,8 @@
 import { Router } from 'express';
-import bcrypt from 'bcrypt';
-import db from '../dbStrategy/mongo.js';
+import { signUp } from '../controllers/authControllers.js';
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", async (req, res) => {
-    const user = req.body;
-    const passwordHash = bcrypt.hashSync(user.password, 10);
-
-    try {
-        await db.collection('users').insertOne({
-            name: user.name,
-            email: user.email,
-            password: passwordHash
-        });
-        res.sendStatus(201);
-    }
-    catch(error) {
-        res.sendStatus(500);
-    }
-
-});
-
+authRouter.post("/sign-up", signUp);
 
 export default authRouter;
